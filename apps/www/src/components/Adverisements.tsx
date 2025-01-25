2523f90f098764b2a49ad5bc4e68b68b78276d9b
kms0219kms <kms0219kms@naver.com>
2025-01-25 22:06:28 +0700

    :fire: feat: 광고에는 캐러셀 버튼 없애기, 슬라이드 못하게

--- apps/www/src/components/Adverisements.tsx
+++ apps/www/src/components/Adverisements.tsx
@@ -1,40 +1,31 @@
 "use client"
 
 import { Swiper, SwiperSlide } from "swiper/react"
-import { Autoplay, Navigation, Pagination } from "swiper/modules"
+import { Autoplay } from "swiper/modules"
 
 import "swiper/css"
 import "swiper/css/autoplay"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
 
-import "./_styles/advertisements.css"
-
 export default function Advertisements() {
   return (
     <Swiper
-      modules={[Autoplay, Navigation, Pagination]}
+      modules={[Autoplay]}
       className="h-[352px] w-[570px] rounded-xl"
+
       slidesPerView={1}
       spaceBetween={16}
-      speed={400}
+
       loop
+      speed={400}
+      allowTouchMove={false}
+
       autoplay={{
         delay: 5000,
         disableOnInteraction: false,
         pauseOnMouseEnter: true,
       }}
-      pagination={{
-        clickable: true,
-        el: ".swiper-pagination",
-        type: "fraction",
-      }}
-      navigation={{
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      }}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={swiper => console.log(swiper)}
     >
       <SwiperSlide>
         <div className="inline-flex size-full items-center justify-center rounded-xl bg-gradient-to-r from-[#132b75] to-ceruleanBlue-700">
@@ -67,20 +58,6 @@ export default function Advertisements() {
           </span>
         </div>
       </SwiperSlide>
-
-      <div className="swiper-indicator">
-        <div className="swiper-pagination"></div>
-
-        <div className="swiper-navigation">
-          <button type="button" className="swiper-button-prev">
-            <span className="sr-only">이전</span>
-          </button>
-
-          <button type="button" className="swiper-button-next">
-            <span className="sr-only">다음</span>
-          </button>
-        </div>
-      </div>
     </Swiper>
   )
 }
