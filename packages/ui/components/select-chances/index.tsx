7350be359a7183e3f4f3d64ea4dd42ea9039d72f
kms0219kms <kms0219kms@naver.com>
2025-02-01 02:18:48 +0700

    :ambulance: hotfix: 모바일에서 경쟁률 표시 이상함

--- packages/ui/components/select-chances/index.tsx
+++ packages/ui/components/select-chances/index.tsx
@@ -53,7 +53,7 @@ export default function SelectChances() {
   ]
 
   return (
-    <div className="flex h-full w-full flex-col justify-between">
+    <div className="flex min-h-full w-full flex-col justify-between">
       <Suspense
         fallback={Array.from({ length: 9 }).map((_, i) => (
           <Skeleton
