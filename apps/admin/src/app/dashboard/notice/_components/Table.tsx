d1357b9ed985f4020b1ff890a7eab2345e614499
kms0219kms <kms0219kms@naver.com>
2025-02-18 23:24:03 +0700

    :construction: feat: dump commit

---
+++ apps/admin/src/app/dashboard/notice/_components/Table.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import { useState } from "react"
+
+import { TrashIcon } from "@heroicons/react/24/outline"
+
+import { Button, NextLink } from "@packages/ui/components/krds/Action"
+import Checkbox from "@packages/ui/components/Checkbox"
+
+export default function NoticeListTable() {
+  const [checkedNotices, setCheckedNotices] = useState<number[]>([])
+
+  return (
+    <div className="flex flex-col gap-24">
+      <table className="w-full border-collapse border-spacing-x-12 border-b border-gray-700">
+        <thead className="h-10 border-b border-gray-500 align-top">
+          <tr>
+            <th scope="col" className="w-[18px] pr-7">
+              <Checkbox
+                id="toggle-all"
+                checked={
+                  checkedNotices.length > 0 &&
+                  // TODO: checkedNotices.length === notices.length
+                  checkedNotices.length === 2
+                }
+                onChange={e => {
+                  const { checked } = e.target
+
+                  if (checked) {
+                    setCheckedNotices([...checkedNotices, ...[1, 2]])
+                  } else {
+                    setCheckedNotices([])
+                  }
+                }}
+              />
+            </th>
+
+            <th scope="col" className="w-auto pr-7 text-start">
+              제목
+            </th>
+
+            <th scope="col" className="w-64 text-right">
+              작성일
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td className="w-[18px] py-6 pr-7">
+              <Checkbox
+                id="toggle-1"
+                checked={checkedNotices.includes(1)}
+                onChange={e => {
+                  const { checked } = e.target
+
+                  if (checked) {
+                    setCheckedNotices([...checkedNotices, 1])
+                  } else {
+                    setCheckedNotices(
+                      checkedNotices.filter(id => id !== 1),
+                    )
+                  }
+                }}
+              />
+            </td>
+
+            <td className="w-auto py-6 pr-7 text-start text-2xl font-bold">
+              <NextLink href="/dashboard/notice/1">Title</NextLink>
+            </td>
+
+            <td className="w-64 py-6 text-right text-sm text-gray-300">
+              2025년 1월 21일 00:05:08
+            </td>
+          </tr>
+
+          <tr>
+            <td className="w-[18px] py-6 pr-7">
+              <Checkbox
+                id="toggle-2"
+                checked={checkedNotices.includes(2)}
+                onChange={e => {
+                  const { checked } = e.target
+
+                  if (checked) {
+                    setCheckedNotices([...checkedNotices, 2])
+                  } else {
+                    setCheckedNotices(
+                      checkedNotices.filter(id => id !== 2),
+                    )
+                  }
+                }}
+              />
+            </td>
+
+            <td className="w-auto py-6 pr-7 text-start text-2xl font-bold">
+              <NextLink href="/dashboard/notice/2">Title</NextLink>
+            </td>
+
+            <td className="w-64 py-6 text-right text-sm text-gray-300">
+              2025년 1월 21일 00:05:08
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <Button
+        type="button"
+        className="w-fit border-point-500 bg-point-500 hover:bg-point-400 focus:bg-point-400 focus:outline-point-500 active:bg-point-400 disabled:cursor-not-allowed disabled:border-point-700 disabled:bg-point-700"
+        disabled={checkedNotices.length <= 0}
+      >
+        <TrashIcon className="size-5 stroke-gray-100 stroke-2" />
+        <span className="text-gray-100">선택한 항목 삭제</span>
+      </Button>
+    </div>
+  )
+}
--- apps/admin/src/app/dashboard/notice/page.tsx
+++ apps/admin/src/app/dashboard/notice/page.tsx
@@ -1,4 +1,9 @@
 import type { Metadata } from "next"
+import { PencilIcon } from "@heroicons/react/24/solid"
+
+import { Button } from "@packages/ui/components/krds/Action"
+
+import NoticeListTable from "./_components/Table"
 
 export const metadata: Metadata = {
   title: "공지사항 관리",
@@ -11,5 +16,23 @@ export const metadata: Metadata = {
 }
 
 export default function Notice() {
-  return <div />
+  return (
+    <div>
+      <div className="mt-10 mb-15 flex items-center justify-between">
+        <h1 className="text-3xl font-bold text-gray-100">
+          공지사항 관리
+        </h1>
+
+        <Button
+          type="button"
+          className="border-gray-100 bg-gray-100 hover:bg-gray-200 focus:bg-gray-200 focus:outline-gray-100 active:bg-gray-200 disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-300"
+        >
+          <PencilIcon className="size-4 fill-gray-900" />
+          <span className="text-gray-900">새로 만들기</span>
+        </Button>
+      </div>
+
+      <NoticeListTable />
+    </div>
+  )
 }
