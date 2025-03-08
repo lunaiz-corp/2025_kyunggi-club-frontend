575df343e293d434ae2043872c4772864919d1ce
Minsu Kim <67222970+kms0219kms@users.noreply.github.com>
2025-03-08 22:04:04 +0700

    :bug: fix: 공지사항 삐져나옴

--- apps/www/src/app/_components/notices/List.tsx
+++ apps/www/src/app/_components/notices/List.tsx
@@ -38,7 +38,7 @@ export default function SummaryList() {
         ) : (
           noticeList
             .sort((a, b) => b.id - a.id)
-            .slice(0, 8)
+            .slice(0, 6)
             .map(x => {
               return (
                 <NoticesSummary
